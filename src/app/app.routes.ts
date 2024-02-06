c894538348bc25f4274b66d4c7c7c00c6c86c28f
Tom <tom.petri7@gmail.com>
2024-02-06 16:51:55 +0100

    Added the remaining Styles to login Component and Added footer Component with all Styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent },
+    { path: 'login', title: 'DABubble - Login', component: LoginComponent },
 ];
