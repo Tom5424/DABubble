31e3b7bd4f9cc919029240d49aced7f9481dab62
Tom <tom.petri7@gmail.com>
2024-02-09 15:38:47 +0100

    You can now select one of the default Avatars

--- src/app/components/avatar-picker/avatar-picker.component.ts
+++ src/app/components/avatar-picker/avatar-picker.component.ts
@@ -2,18 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterLink } from '@angular/router';
 import { FormDataService } from '../../services/form-data.service';
+import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-avatar-picker',
   standalone: true,
-  imports: [FooterComponent, RouterLink],
+  imports: [FooterComponent, RouterLink, NgClass],
   templateUrl: './avatar-picker.component.html',
   styleUrl: './avatar-picker.component.scss'
 })
 
 
 export class AvatarPickerComponent implements OnInit {
+  avatarImageUrls: string[] = [
+    './assets/img/avatar-1.svg',
+    './assets/img/avatar-2.svg',
+    './assets/img/avatar-3.svg',
+    './assets/img/avatar-4.svg',
+    './assets/img/avatar-5.svg',
+    './assets/img/avatar-6.svg',
+  ];
+  selectedImageAvatarUrl: string = '';
 
 
   constructor(public formDataService: FormDataService) {
@@ -24,6 +34,15 @@ export class AvatarPickerComponent implements OnInit {
 
   ngOnInit(): void {
     this.formDataService.loadFormDataSignupForm();
+  }
+
+
+  selectOrToggleAvatar(selectedImageAvatarUrl: string): void {
+    if (this.selectedImageAvatarUrl !== selectedImageAvatarUrl) {
+      this.selectedImageAvatarUrl = selectedImageAvatarUrl;
+    } else {
+      this.selectedImageAvatarUrl = '';
+    }
   }
 
 
