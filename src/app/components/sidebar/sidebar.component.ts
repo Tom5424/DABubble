d9ac0d6d9aab5ad39561aa2bdadea42efa7213af
Tom <tom.petri7@gmail.com>
2024-02-27 17:31:58 +0100

    The Channel List and Contact List can collapsed in the Sidebar Componnet

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,27 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 
 
 export class SidebarComponent {
+  channelsAreCollapsed: boolean = false;
+  contactsAreCollapsed: boolean = false;
 
+
+  foldInChannels(): void {
+    this.channelsAreCollapsed = !this.channelsAreCollapsed;
+  }
+
+
+  foldInContacts(): void {
+    this.contactsAreCollapsed = !this.contactsAreCollapsed;
+  }
 }
