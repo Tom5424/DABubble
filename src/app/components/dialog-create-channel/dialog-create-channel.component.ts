b7c080f7d497c5ea452b7b0f8064defd153117d7
Tom <tom.petri7@gmail.com>
2024-02-28 14:28:29 +0100

    Added Form Validation to Dialog create Channel Component

--- src/app/components/dialog-create-channel/dialog-create-channel.component.ts
+++ src/app/components/dialog-create-channel/dialog-create-channel.component.ts
@@ -1,16 +1,46 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialogClose } from '@angular/material/dialog';
 
 
 @Component({
   selector: 'app-dialog-create-channel',
   standalone: true,
-  imports: [MatDialogClose],
+  imports: [MatDialogClose, ReactiveFormsModule, NgClass],
   templateUrl: './dialog-create-channel.component.html',
   styleUrl: './dialog-create-channel.component.scss'
 })
 
 
 export class DialogCreateChannelComponent {
+  createChannelForm = new FormGroup({
+    channelName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+    channelDescription: new FormControl('', Validators.maxLength(150)),
+  })
 
+
+  inputFieldChannelNameIsRequired(): boolean {
+    return (this.createChannelForm.controls.channelName.touched && this.createChannelForm.controls.channelName.errors?.['required']) ? true : false;
+  }
+
+
+  inputfieldChannelNameIsToShort(): boolean {
+    return (this.createChannelForm.controls.channelName.errors?.['minlength']) ? true : false;
+  }
+
+
+  inputfieldChannelNameIsToLong(): boolean {
+    return (this.createChannelForm.controls.channelName.errors?.['maxlength']) ? true : false;
+  }
+
+
+  inputfieldChannelDescriptionIsToLong(): boolean {
+    return (this.createChannelForm.controls.channelDescription.errors?.['maxlength']) ? true : false;
+  }
+
+
+  openDialogAddPeople() {
+
+  }
 }
