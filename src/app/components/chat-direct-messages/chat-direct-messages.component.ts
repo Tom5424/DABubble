693de45609c999f2e3c0c738d0358f4de8e3f1c3
Tom <tom.petri7@gmail.com>
2024-03-08 15:52:28 +0100

    Added remaining Styles to chat-direct-messages Component

--- src/app/components/chat-direct-messages/chat-direct-messages.component.ts
+++ src/app/components/chat-direct-messages/chat-direct-messages.component.ts
@@ -13,4 +13,24 @@ import { QuillModule } from 'ngx-quill';
 
 export class ChatDirectMessagesComponent {
 
+
+  focusQuillEditor(event: { editor: any, range: any, oldRange: any }): void {
+    if (this.quillEditorIsFocused(event)) {
+      event.editor.container.style.border = '1px solid #535AF1';
+      event.editor.container.style.color = '#000000';
+    } else if (this.quillEditorIsNotFocused(event)) {
+      event.editor.container.style.border = '1px solid #adb0d9';
+      event.editor.container.style.color = '#686868';
+    }
+  }
+
+
+  quillEditorIsFocused(event: { editor: any, range: any, oldRange: any }): boolean {
+    return (event.oldRange == null) ? true : false;
+  }
+
+
+  quillEditorIsNotFocused(event: { editor: any, range: any, oldRange: any }): boolean {
+    return (event.range == null) ? true : false;
+  }
 }
