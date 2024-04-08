95d03d4f5e29669c90fb36e6253e2823d408206b
Tom <tom.petri7@gmail.com>
2024-04-08 12:54:31 +0200

    Added the remaining Styles to the dialog-edit-channel Component. And added Form Validation to dialog-edit-channel Component.

--- src/app/components/dialog-edit-channel/dialog-edit-channel.component.ts
+++ src/app/components/dialog-edit-channel/dialog-edit-channel.component.ts
@@ -1,16 +1,65 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 
 
 @Component({
   selector: 'app-dialog-edit-channel',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, ReactiveFormsModule, NgClass],
   templateUrl: './dialog-edit-channel.component.html',
   styleUrl: './dialog-edit-channel.component.scss'
 })
 
 
 export class DialogEditChannelComponent {
+  channelNameIsInEditMode: boolean = false;
+  channelDescriptionIsInEditMode: boolean = false;
+  editChannelForm = new FormGroup({
+    channelName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15)]),
+    channelDescription: new FormControl('', Validators.maxLength(150)),
+  })
 
+
+  inputFieldChannelNameIsRequired(): boolean {
+    return (this.editChannelForm.controls.channelName.touched && this.editChannelForm.controls.channelName.errors?.['required']) ? true : false;
+  }
+
+
+  inputfieldChannelNameIsToShort(): boolean {
+    return (this.editChannelForm.controls.channelName.errors?.['minlength']) ? true : false;
+  }
+
+
+  inputfieldChannelNameIsToLong(): boolean {
+    return (this.editChannelForm.controls.channelName.errors?.['maxlength']) ? true : false;
+  }
+
+
+  inputfieldChannelDescriptionIsToLong(): boolean {
+    return (this.editChannelForm.controls.channelDescription.errors?.['maxlength']) ? true : false;
+  }
+
+
+  editChannelName(): void {
+    this.channelNameIsInEditMode = true;
+  }
+
+
+  saveChangesToEditChannelName(): void {
+    this.channelNameIsInEditMode = false;
+    this.editChannelForm.controls.channelName.reset();
+  }
+
+
+  editChannelDescription(): void {
+    this.channelDescriptionIsInEditMode = true;
+  }
+
+
+  saveChangesToEditChannelDescription(): void {
+    this.channelDescriptionIsInEditMode = false;
+    this.editChannelForm.controls.channelDescription.reset();
+  }
 }
