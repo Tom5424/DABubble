0e8e4e48f37e71749d91f5e0fc01b11cccd9c4ba
Tom <tom.petri7@gmail.com>
2024-04-30 19:04:44 +0200

    Added mat Menu to header-mobile-view Component

--- src/app/components/header-mobile-view/header-mobile-view.component.ts
+++ src/app/components/header-mobile-view/header-mobile-view.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+import { DialogProfileDetailViewComponent } from '../dialog-profile-detail-view/dialog-profile-detail-view.component';
+import { MatDialog } from '@angular/material/dialog';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-header-mobile-view',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule, RouterLink],
   templateUrl: './header-mobile-view.component.html',
   styleUrls: ['./header-mobile-view.component.scss', './header-mobile-view.component.media.scss'],
 })
@@ -13,6 +17,8 @@ import { AuthService } from '../../services/auth.service';
 
 export class HeaderMobileViewComponent implements OnInit {
   authService = inject(AuthService);
+  matDialog = inject(MatDialog);
+  @ViewChild('matMenuTrigger') matMenuTrigger!: MatMenuTrigger;
 
 
   ngOnInit(): void {
@@ -38,5 +44,22 @@ export class HeaderMobileViewComponent implements OnInit {
 
   userIsOnline(): boolean {
     return (this.authService.user.isOnline) ? true : false;
+  }
+
+
+  openProfileDetailView(): void {
+    this.matDialog.open(DialogProfileDetailViewComponent, { position: { top: '95px', right: '25px' } });
+  }
+
+
+  logout(): void {
+    this.authService.logoutService();
+  }
+
+
+  closeMatMenuIfInMobileView(): void {
+    if (window.innerWidth <= 540) {
+      this.matMenuTrigger.closeMenu();
+    }
   }
 }
